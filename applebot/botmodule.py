3c060eb85cdea0f58821373a3327126e9acbb34d
Apple <appul@users.noreply.github.com>
2016-08-26 00:16:55 +0200

    Eh...

--- applebot/botmodule.py
+++ applebot/botmodule.py
@@ -14,17 +14,16 @@ class BotModule(object):
         self.__register_handlers()
 
     def __register_handlers(self):
-        for method in [getattr(self, m) for m in dir(self) if '__' not in m]:
-            if callable(method):
-                commands = method.__dict__.get('command_names', ())
-                for command in commands:
-                    log.debug('Adding command \'{}\' in {}'.format(commands, self.__class__.__name__))
-                    self.client.commands.add(str(command), method)
+        for method in filter(callable, [getattr(self, m) for m in dir(self) if '__' not in m]):
+            commands = method.__dict__.get('command_names', ())
+            for command in commands:
+                log.debug('Adding command \'{}\' in {}'.format(commands, self.__class__.__name__))
+                self.client.commands.add(str(command), method)
 
-                events = method.__dict__.get('event_names', ())
-                for event in events:
-                    log.debug('Registering event \'{}\' from {}'.format(event, self.__class__.__name__))
-                    self.client.events.add(event, method)
+            events = method.__dict__.get('event_names', ())
+            for event in events:
+                log.debug('Registering event \'{}\' from {}'.format(event, self.__class__.__name__))
+                self.client.events.add(event, method)
 
     @staticmethod
     def event(*events):
